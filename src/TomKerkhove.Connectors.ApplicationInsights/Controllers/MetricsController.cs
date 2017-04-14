3fb1d06e56180ffb03d614efcdd34329b558ed01
Tom Kerkhove <kerkhove.tom@gmail.com>
2017-04-14 18:05:31 +0200

    Add boilerplate

---
+++ src/TomKerkhove.Connectors.ApplicationInsights/Controllers/MetricsController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace TomKerkhove.Connectors.ApplicationInsights.Controllers
+{
+    [RoutePrefix("api/v1")]
+    public class MetricsController : ApiController
+    {
+        [HttpPost]
+        [Route("metics")]
+        public async Task<IHttpActionResult> WriteMetric(string metricName)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ src/TomKerkhove.Connectors.ApplicationInsights/Controllers/TelemetryController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace TomKerkhove.Connectors.ApplicationInsights.Controllers
+{
+    [RoutePrefix("api/v1")]
+    public class TelemetryController : ApiController
+    {
+        [HttpPost]
+        [Route("telemetry")]
+        public async Task<IHttpActionResult> WriteTrace()
+        {
+            return Ok();
+        }
+    }
+}
